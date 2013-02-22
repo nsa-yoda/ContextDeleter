5b6e46d1d43e0089bfc314810168bf62f79286f8
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2013-02-22 14:22:50 -0500

    Initial commit

---
+++ ContextDeleterDev/background.js
@@ -0,0 +1,8 @@
+﻿var e = chrome.contextMenus.create({
+    "type":"normal", 
+    "title":"Delete This Element", 
+    "contexts":["all", "page", "frame", "selection", "link", "editable", "image","video", "audio"], 
+    "onclick": function(info, tab){
+      chrome.tabs.sendRequest(tab.id, "deleteElement")
+    } 
+});
---
+++ ContextDeleterDev/content.js
@@ -0,0 +1,29 @@
+﻿var clickedEl = null;
+var oldClickedEl = null;
+
+// Add event listener on right click
+document.addEventListener("mousedown", function(event){
+    if(event.button == 2){
+      // Remove the border from the old clickedEl
+      $(oldClickedEl).css("border","none");
+        
+      // Which El was clicked?
+      clickedEl = event.target;
+      
+      // Add a border to the clickedEl
+      $(event.target).css("border","1px solid blue")
+      
+      // Make a copy of the clickedEl
+      oldClickedEl = clickedEl;
+    }
+    
+    if(event.button == 0 || event.button == 1){
+      $(clickedEl, oldClickedEl).css("border","none")
+    }
+}, true);
+
+// Add onRequest response
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    if(request == "deleteElement")
+        $(clickedEl).remove()
+});

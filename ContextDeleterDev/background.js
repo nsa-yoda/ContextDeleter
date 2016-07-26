9166d07b2069ab844c2c48efa9ae129086f1f80e
juan.sanchez <juan.sanchez@borderfree.com>
2016-07-26 16:56:46 -0400

    Restructure

--- ContextDeleterDev/background.js
+++
@@ -1,8 +0,0 @@
-﻿var e = chrome.contextMenus.create({
-    "type":"normal", 
-    "title":"Delete This Element", 
-    "contexts":["all", "page", "frame", "selection", "link", "editable", "image","video", "audio"], 
-    "onclick": function(info, tab){
-      chrome.tabs.sendRequest(tab.id, "deleteElement")
-    } 
-});

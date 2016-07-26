7c011cc2c0232fcca8b8f09c682a839d8703091d
juan.sanchez <juan.sanchez@borderfree.com>
2016-07-26 17:00:22 -0400

    background.js beautify

--- background.js
+++ background.js
@@ -1,7 +1,17 @@
 ﻿var e = chrome.contextMenus.create({
-    "type":"normal", 
-    "title":"Delete This Element", 
-    "contexts":["all", "page", "frame", "selection", "link", "editable", "image","video", "audio"], 
+    "type": "normal",
+    "title": "Delete This Element",
+    "contexts": [
+        "all",
+        "page",
+        "frame",
+        "selection",
+        "link",
+        "editable",
+        "image",
+        "video",
+        "audio"
+    ],
     "onclick": function(info, tab){
       chrome.tabs.sendRequest(tab.id, "deleteElement")
     } 

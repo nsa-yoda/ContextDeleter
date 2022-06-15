ce2297f3239611286bb2cef4f9f47962bcdea56b
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2022-06-15 10:43:11 -0400

    updates

--- background.js
+++ background.js
@@ -2,14 +2,14 @@
  * workaround as chrome.i18n.getMessage is not available in service workers
  */
 const getMessage = async function (message) {
-  const locale = navigator.language.split('-')[0];
+  let locale = navigator.language.split('-')[0];
 
   // hardcoded locale bounding for the locales we support
   // this must be updated when we add a new locale
   const _locales = ['en', 'es', 'fr', 'gb'];
   if (!_locales.includes(locale)) {
-    console.log("ContextDeleter: Locale not found in Locales")
-    return "";
+    console.log("ContextDeleter: Locale not found in Locales, defaulting to English")
+    locale = "en";
   }
 
   const jsonLocalFile = `_locales/${locale}/messages.json`;
@@ -26,6 +26,23 @@ const getMessage = async function (message) {
     });
 }
 
+const ensureSendMessage = function(tabId, message, callback){
+  chrome.tabs.sendMessage(tabId, {ping: true}, function(response){
+    if(response && response.pong) { // Content script ready
+      chrome.tabs.sendMessage(tabId, message, callback);
+    } else { // No listener on the other end
+      chrome.tabs.executeScript(tabId, {file: "content.js"}, function(){
+        if(chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+          throw Error("Unable to inject script into tab " + tabId);
+        }
+        // OK, now it's injected and ready
+        chrome.tabs.sendMessage(tabId, message, callback);
+      });
+    }
+  });
+}
+
 /**
  * Add creation listeners on installed
  */
@@ -36,21 +53,30 @@ chrome.runtime.onInstalled.addListener(async function () {
     'title': await getMessage('openContextMenuTitle'),
     'contexts': ['all']
   });
-});
 
-chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-  chrome.tabs.sendMessage(tab.id, {'action': 'deleteElement'}, function (response) {
-    console.log("got response", response)
+  chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    console.log("message", message)
+    console.log("sender", sender)
+    console.log("sendResponse", sendResponse)
+    if (message.action) {
+      if (message.action === "deleteElement") {
+        $(clickedElement).remove();
+        sendResponse({value: "ok"})
+      }
+    }
+  });
+
+  chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+    console.log("Sending message action deleteelement to tab " + tab.id)
+    chrome.tabs.sendMessage(tab.id, {'action': 'deleteElement'}, function (response) {
+      if (!chrome.runtime.lastError) {
+        // message processing code goes here
+      } else {
+        console.log(chrome.runtime.lastError)
+        // error handling code goes here
+      }
+      
+      console.log("got response", response)
+    });
   });
 });
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(request.action)
-  if (request.action) {
-    if (request.action === "deleteElement") {
-      $(clickedElement).remove();
-      sendResponse({value: "ok"})
-    }
-  }
-});
-
